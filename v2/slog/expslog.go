7c513e962af5876b53400c7e25641565b851b185
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-08-08 20:56:35 +0200

    fix slog

--- v2/slog/expslog.go
+++ v2/slog/expslog.go
@@ -41,12 +41,12 @@ func String(k, v string) slog.Attr                             { return slog.Str
 func StringValue(value string) slog.Value                      { return slog.StringValue(value) }
 func NewJSONHandler(w io.Writer, opts *HandlerOptions) Handler { return slog.NewJSONHandler(w, opts) }
 
-func Any(k string, v any) slog.Attr                  { return slog.Any(k, v) }
-func Bool(k string, v bool) slog.Attr                { return slog.Bool(k, v) }
-func Duration(key string, v time.Duration) slog.Attr { return slog.Duration(k, v) }
-func Float64(key string, v float64) slog.Attr        { return slog.Float64(k, v) }
-func Group(key string, args ...any) slog.Attr        { return slog.Group(k, args...) }
-func Int(k string, v int) slog.Attr                  { return slog.Int(k, v) }
-func Int64(k string, v int64) slog.Attr              { return slog.Int64(k, v) }
-func Time(k string, v time.Time) slog.Attr           { return slog.Time(k, v) }
-func Uint64(k string, v uint64) slog.Attr            { return slog.Int64(k, v) }
+func Any(k string, v any) slog.Attr                { return slog.Any(k, v) }
+func Bool(k string, v bool) slog.Attr              { return slog.Bool(k, v) }
+func Duration(k string, v time.Duration) slog.Attr { return slog.Duration(k, v) }
+func Float64(k string, v float64) slog.Attr        { return slog.Float64(k, v) }
+func Group(k string, args ...any) slog.Attr        { return slog.Group(k, args...) }
+func Int(k string, v int) slog.Attr                { return slog.Int(k, v) }
+func Int64(k string, v int64) slog.Attr            { return slog.Int64(k, v) }
+func Time(k string, v time.Time) slog.Attr         { return slog.Time(k, v) }
+func Uint64(k string, v uint64) slog.Attr          { return slog.Uint64(k, v) }
