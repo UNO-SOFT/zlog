cec821aafaee9bf41f4ab50500085abadfc96170
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2023-08-15 14:20:35 +0200

    Add handler Group test

--- v2/handlers_test.go
+++ v2/handlers_test.go
@@ -6,8 +6,11 @@ package zlog_test
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"io"
 	"testing"
+	"time"
 
 	"github.com/UNO-SOFT/zlog/v2"
 	"github.com/UNO-SOFT/zlog/v2/slog"
@@ -74,4 +77,61 @@ func TestMultiHandlerLevel(t *testing.T) {
 	) {
 		return
 	}
+}
+
+func TestGroup(t *testing.T) {
+	do := func(logger *slog.Logger) {
+		logger.Info("naked", "a", 0)
+		{
+			logger := logger.WithGroup("group")
+			logger.Info("justGroup", "a", 1)
+		}
+		logger = logger.With("with", "value")
+		logger.Info("withValue", "a", 2)
+		logger = logger.WithGroup("group")
+		logger.Info("withValueGroup", "a", 3)
+	}
+
+	t.Run("console", func(t *testing.T) {
+		verbose := zlog.VerboseVar(2)
+		var buf bytes.Buffer
+		zl := zlog.NewConsoleHandler(&verbose, &buf)
+		logger := zlog.NewLogger(zl).SLog()
+
+		do(logger)
+		t.Log(buf.String())
+	})
+
+	t.Run("json", func(t *testing.T) {
+		var buf bytes.Buffer
+		zl := zlog.DefaultHandlerOptions.NewJSONHandler(&buf)
+		logger := zlog.NewLogger(zl).SLog()
+
+		do(logger)
+		t.Log(buf.String())
+		type Line struct {
+			Time   time.Time      `json:"time"`
+			Level  string         `json:"level"`
+			Msg    string         `json:"msg"`
+			Source string         `json:"source"`
+			Group  map[string]any `json:"group"`
+			A      int            `json:"a"`
+			With   string         `json:"with"`
+		}
+		dec := json.NewDecoder(bytes.NewReader(buf.Bytes()))
+		for {
+			var line Line
+			if err := dec.Decode(&line); err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				t.Fatal(err)
+			}
+			t.Log(line)
+			if line.Source == "" {
+				t.Error("no source")
+			}
+		}
+
+	})
 }
